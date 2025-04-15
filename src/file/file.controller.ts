a93d9b48262440ea020a0fda3ed1cefae2593b77
Jeffrey Tan <jtjeffreytan@gmail.com>
2025-04-15 15:00:43 +1000

    feat: add some multer stuff

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  UseInterceptors,
+  UseGuards,
+  UploadedFile,
+} from '@nestjs/common';
 import { FileService } from './file.service';
 import { ParseUUIDV4Pipe } from '@/common/utils/parse-uuid-v4.pipe';
 import { CreateFileDto } from './dto/create-file.dto';
 import { AddFileVersionDto } from './dto/add-file-version.dto';
+import { DummyGuard } from '@/auth/dummy/dummy.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
+@UseGuards(DummyGuard)
 @Controller('file')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
@@ -19,16 +31,28 @@ export class FileController {
   }
 
   @Post()
-  create(@Body() createFileDto: CreateFileDto) {
+  @UseInterceptors(FileInterceptor('file'))
+  create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() createFileDto: CreateFileDto,
+  ) {
     // return this.fileService.create(createFileDto);
   }
 
   @Post(':id/version')
+  @UseInterceptors(FileInterceptor('file'))
   addNewVersion(
     @Param('id', new ParseUUIDV4Pipe()) id: string,
+    @UploadedFile() file: Express.Multer.File,
     @Body() addFileVersionDto: AddFileVersionDto,
   ) {
-    // return this.fileService.addNewVersion(id, addFileVersionDto);
+    return this.fileService.addNewVersion(
+      id,
+      '123',
+      file.buffer,
+      addFileVersionDto.metadata,
+      addFileVersionDto.storageStrategy,
+    );
   }
 
   //TODO: add body
@@ -37,11 +61,11 @@ export class FileController {
     @Param('id', new ParseUUIDV4Pipe()) id: string,
     @Param('versionId', new ParseUUIDV4Pipe()) versionId: string,
   ) {
-    // return this.fileService.restoreVersion(id, versionId);
+    return this.fileService.restoreVersion(id, '123', versionId);
   }
 
-  @Delete(':id')
-  delete(@Param('id', new ParseUUIDV4Pipe()) id: string) {
-    return this.fileService.delete(id);
+  @Post(':id/delete')
+  async softDelete(@Param('id', new ParseUUIDV4Pipe()) id: string) {
+    await this.fileService.softDelete(id, '123');
   }
 }
